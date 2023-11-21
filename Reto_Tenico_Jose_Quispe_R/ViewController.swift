1850c20b641483375c760896e8a74d64aab5dd19
Quispe Rodriguez José Leoncio <jose.quispe_stratis@scotiabank.com.pe>
2023-11-21 18:00:51 -0500

    Initial Commit

---
+++ Reto_Tenico_Jose_Quispe_R/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reto_Tenico_Jose_Quispe_R
+//
+//  Created by MAC9STRATIS001 on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
